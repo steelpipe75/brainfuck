3dbe0569a082483f41dde6df4c40d62dc8fea95c
Y.N <kazumiya50@hotmail.com>
2013-08-03 20:54:16 +0900

    正常系のテストが通るように修正

--- c/Source/brainfuck.c
+++ c/Source/brainfuck.c
@@ -1,12 +1,30 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "brainfuck.h"
+
+typedef struct BFINTERPRETER{
+	int tapesize;
+}BFINTERPRETER;
+
 
 BFI *brainfuck_new(int tapesize)
 {
-	return NULL;
+	BFI *bfi = NULL;
+	
+	if(0 != tapesize){
+		bfi = malloc(sizeof(BFINTERPRETER));
+	}
+	
+	return bfi;
 }
 
 int brainfuck_delete(BFI bfi)
 {
-	return BFI_ERROR;
+	int Ret = BFI_ERROR;
+	
+	if(NULL != bfi){
+		free(bfi);
+		Ret = BFI_SUCCESS;
+	}
+	
+	return Ret;
 }
