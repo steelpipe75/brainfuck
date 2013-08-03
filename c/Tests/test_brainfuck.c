786115b7e719b61599703cbfbcb2d58ba3306394
Y.N <kazumiya50@hotmail.com>
2013-08-03 20:52:34 +0900

    異常系、正常系でテスト分ける

--- c/Tests/test_brainfuck.c
+++ c/Tests/test_brainfuck.c
@@ -2,7 +2,7 @@
 
 #include "brainfuck.h"
 
-void test_NewDelete(void)
+void test_NewDelete_error(void)
 {
 	BFI *bfi;
 	int bfi_ret;
@@ -16,9 +16,24 @@ void test_NewDelete(void)
 	PCU_ASSERT_EQUAL(BFI_ERROR, bfi_ret);
 }
 
+void test_NewDelete(void)
+{
+	BFI *bfi;
+	int bfi_ret;
+	
+	bfi = brainfuck_new(256);
+	
+	PCU_ASSERT_PTR_NOT_EQUAL(NULL, bfi);
+	
+	bfi_ret = brainfuck_delete(bfi);
+	
+	PCU_ASSERT_EQUAL(BFI_SUCCESS, bfi_ret);
+}
+
 PCU_Suite *NewDeleteTest_suite(void)
 {
 	static PCU_Test tests[] = {
+		PCU_TEST(test_NewDelete_error),
 		PCU_TEST(test_NewDelete),
 	};
 	static PCU_Suite suite = { "NewDeleteTest", tests, ( sizeof(tests) / sizeof(tests[0]) ) };
