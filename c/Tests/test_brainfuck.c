f3204e46cb45b7b24c6e40f7148a385c4af227c2
Y.N <kazumiya50@hotmail.com>
2013-08-03 20:30:14 +0900

    初期化テストで一まとめにする

--- c/Tests/test_brainfuck.c
+++ c/Tests/test_brainfuck.c
@@ -2,29 +2,22 @@
 
 #include "brainfuck.h"
 
-void test_new(void)
+void test_NewDelete(void)
 {
 	BFI *bfi;
-	
-	bfi = brainfuck_new(0);
-	
-	PCU_ASSERT_PTR_EQUAL(NULL, bfi);
-}
-
-void test_delete(void)
-{
 	int bfi_ret;
 	
+	bfi = brainfuck_new(0);
 	bfi_ret = brainfuck_delete(NULL);
 	
+	PCU_ASSERT_PTR_EQUAL(NULL, bfi);
 	PCU_ASSERT_EQUAL(BFI_ERROR, bfi_ret);
 }
 
 PCU_Suite *NewDeleteTest_suite(void)
 {
 	static PCU_Test tests[] = {
-		PCU_TEST(test_new),
-		PCU_TEST(test_delete),
+		PCU_TEST(test_NewDelete),
 	};
 	static PCU_Suite suite = { "NewDeleteTest", tests, ( sizeof(tests) / sizeof(tests[0]) ) };
 	return &suite;
