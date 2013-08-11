0c2982972daa91fbd2c1f75033be6e803ee112e5
Y.N <kazumiya50@hotmail.com>
2013-08-11 17:26:36 +0900

    分類分けのための空行追加

--- c/Source/brainfuck.h
+++ c/Source/brainfuck.h
@@ -8,11 +8,13 @@ typedef struct BFINTERPRETER* BFI;
 
 extern BFI brainfuck_new(const char *ptr_program, int programsize, int tapesize);
 extern int brainfuck_delete(BFI bfi);
-extern int brainfuck_get_tapesize(BFI bfi, int *ptr_tapesize);
 
+extern int brainfuck_get_tapesize(BFI bfi, int *ptr_tapesize);
 extern int brainfuck_get_tapeptr(BFI bfi, const char **ptr_tapeptr);
+
 extern int brainfuck_get_programsize(BFI bfi, int *ptr_programsize);
 extern int brainfuck_get_programptr(BFI bfi, const char **ptr_programptr);
+
 extern int brainfuck_check_programbracket(const char *ptr_program, int programsize);
 
 #endif /* BRAINFUCK_H_INCLUDED */
